852033ee0c83ac15b3509c92a95a2f59a89401b7
dmitriy-korotov <dmitriy.korotov.76@mail.ru>
2024-09-08 16:58:52 +0300

    Completed task 3 from lab4

--- lab4/src/process_memory.c
+++ lab4/src/process_memory.c
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 /* Below is a macro definition */
-#define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %8X\n", ID, &I))
+#define SHW_ADR(ID, I) (printf("ID %s \t is at virtual address: %10p\n", ID, &I))
 
 extern int etext, edata, end; /* Global variables for process
                                  memory */
@@ -17,23 +17,26 @@ char *cptr = "This message is output by the function showit()\n"; /* Static */
 char buffer1[25];
 int showit(); /* Function prototype */
 
-main() {
+int main() {
   int i = 0; /* Automatic variable */
 
   /* Printing addressing information */
-  printf("\nAddress etext: %8X \n", &etext);
-  printf("Address edata: %8X \n", &edata);
-  printf("Address end  : %8X \n", &end);
+  printf("\nAddress etext: %10p \n", &etext);
+  printf("Address edata: %10p \n", &edata);
+  printf("Address end  : %10p \n", &end);
 
   SHW_ADR("main", main);
   SHW_ADR("showit", showit);
   SHW_ADR("cptr", cptr);
   SHW_ADR("buffer1", buffer1);
   SHW_ADR("i", i);
+  SHW_ADR("strcpy", strcpy);
+  SHW_ADR("write", write);
   strcpy(buffer1, "A demonstration\n");   /* Library function */
   write(1, buffer1, strlen(buffer1) + 1); /* System call */
   showit(cptr);
 
+  return 1;
 } /* end of main function */
 
 /* A function follows */
@@ -42,7 +45,7 @@ int showit(p) char *p;
   char *buffer2;
   SHW_ADR("buffer2", buffer2);
   if ((buffer2 = (char *)malloc((unsigned)(strlen(p) + 1))) != NULL) {
-    printf("Alocated memory at %X\n", buffer2);
+    printf("Alocated memory at %10p\n", buffer2);
     strcpy(buffer2, p);    /* copy the string */
     printf("%s", buffer2); /* Didplay the string */
     free(buffer2);         /* Release location */
